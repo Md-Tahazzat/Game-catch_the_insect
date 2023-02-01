df742827a63c202edaa575f9333420a50d404bef
Md-Tahazzat <mdtahazzatali820@gmail.com>
2023-02-01 20:09:50 +0600

    restart button added

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const gameContainer = document.querySelector("#game_container");
+const endScoreContainer = document.querySelector("#end_score_container");
+const endScore = document.querySelector("#end_score");
 const gameField = document.querySelector("#game_field");
 const insects = document.querySelectorAll(".insects");
 const headerContainer = document.querySelector("#header_container");
 const selectContainer = document.querySelector("#select");
+const time = document.querySelector("#time");
+const score = document.querySelector("#score");
+let totalScore = 0;
+let minutes = 3;
+let seconds = 1;
 
 // playGame.addEventListener("click", () => {
 //   selectContainer.classList.remove("display_none");
@@ -18,6 +25,7 @@ insects.forEach((insect) =>
     gameContainer.classList.remove("hidden");
     selectContainer.classList.add("hidden");
     createInsect();
+    startTime();
   })
 );
 
@@ -51,9 +59,63 @@ function randomDeg() {
   return deg;
 }
 
+// hide selected insect and make double insect
 function display(e) {
   for (let i = 0; i < 2; i++) {
     createInsect();
   }
+  if (totalScore === 30) {
+    contactInfo();
+  }
+  totalScore += 1;
+  score.textContent = totalScore;
   e.classList.add("hidden");
+}
+
+// clock function
+function startTime() {
+  setInterval(() => {
+    if (minutes === 0 && seconds === 1) {
+      clearInterval();
+      endScoreContainer.classList.remove("hidden");
+      endScore.textContent = totalScore;
+      gameContainer.classList.add("hidden");
+    }
+    if (seconds === 1) {
+      seconds = 59;
+      minutes -= 1;
+    } else {
+      seconds -= 1;
+    }
+    let fullTime = `${(minutes <= 9 ? "0" : "") + minutes}:${
+      (seconds < 10 ? "0" : "") + seconds
+    }`;
+    time.textContent = fullTime;
+  }, 1000);
+}
+
+// contact section
+function contactInfo() {
+  const contact = document.createElement("div");
+  contact.innerHTML = `
+    <h1 class="text-center mb-5 text-slate-200 text-2xl">Hi there.....</h1>
+    <h3 class='text-center  text-slate-200 text-xl'>you are playing</h3>
+    <h3 class='text-center mb-4 text-slate-200 text-xl'>the imposible game</h3>
+    <p class='text-slate-200 text-center'>Let me know how far you go...</p>
+    <p class='text-slate-200 text-center'>mdtahazzatali820@gmail.com</p>
+    `;
+  gameField.appendChild(contact);
+}
+
+// Restart the game
+function restartGame() {
+  totalScore = 0;
+  minutes = 3;
+  seconds = 1;
+
+  gameField.innerHTML = "";
+  gameContainer.classList.remove("hidden");
+  endScoreContainer.classList.add("hidden");
+  createInsect();
+  startTime();
 }
